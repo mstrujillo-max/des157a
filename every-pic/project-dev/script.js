f4f30d6ecab3d07dbe16a9f2943f2e9f7baac7c3
Mia Soledad Trujillo <mstrujil@scc2102-06.ucdavis.edu>
2026-05-14 19:22:07 -0700

    in class work

---
+++ every-pic/project-dev/script.js
@@ -0,0 +1,16 @@
+(function(){
+    "use strict";
+    console.log("reading js");
+
+
+    const heartCollege = document.querySelector(".collegePark .heart")
+    const heartMabels = document.querySelector(".mabelsMarket .heart")
+    const heartArb = document.querySelector(".arboretum .heart")
+    const heartVarsity = document.querySelector(".varsity .heart")
+    const heartTemple = document.querySelector(".temple .heart")
+    const heartMercado = document.querySelector(".mercado .heart")
+
+    heartCollege.addEventListener("click",function(){
+        
+    })
+})();
