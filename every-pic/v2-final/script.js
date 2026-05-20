18fe0ae7582a319e93126a021ef53735640d5c84
Mia Trujillo <mileysue2004@gmail.com>
2026-05-20 10:55:49 -0700

    new chages

--- every-pic/v2-final/script.js
+++ every-pic/v2-final/script.js
@@ -103,6 +103,15 @@
         rightSqrCollege.style.opacity="1";
     });
 
+    heartCollege.addEventListener("mouseleave", function() {
+        leftSqrCollege.style.scale = "0%";
+        leftSqrCollege.style.opacity = "0";
+        middleSqrCollege.style.scale = "0%";
+        middleSqrCollege.style.opacity = "0";
+        rightSqrCollege.style.scale = "0%";
+        rightSqrCollege.style.opacity = "0";
+    });
+
     buttonCollege.addEventListener("click",function(){
         collegeOverlay.style.display ="none";
         shelf.style.display="block";
@@ -117,6 +126,15 @@
         middleSqrMabels.style.opacity="1";
         rightSqrMabels.style.scale="100%";
         rightSqrMabels.style.opacity="1";
+    });
+
+    heartMabels.addEventListener("mouseleave", function() {
+        leftSqrMabels.style.scale = "0%";
+        leftSqrMabels.style.opacity = "0";
+        middleSqrMabels.style.scale = "0%";
+        middleSqrMabels.style.opacity = "0";
+        rightSqrMabels.style.scale = "0%";
+        rightSqrMabels.style.opacity = "0";
     });
 
     buttonMabels.addEventListener("click",function(){
@@ -136,6 +154,15 @@
         rightSqrVarsity.style.opacity="1";
     });
 
+    heartVarsity.addEventListener("mouseleave", function() {
+        leftSqrVarsity.style.scale = "0%";
+        leftSqrVarsity.style.opacity = "0";
+        middleSqrVarsity.style.scale = "0%";
+        middleSqrVarsity.style.opacity = "0";
+        rightSqrVarsity.style.scale = "0%";
+        rightSqrVarsity.style.opacity = "0";
+    });
+
     buttonVarsity.addEventListener("click",function(){
         varsityOverlay.style.display ="none";
         shelf.style.display="block";
@@ -153,6 +180,15 @@
         rightSqrTemple.style.opacity="1";
     });
 
+    heartTemple.addEventListener("mouseleave", function() {
+        leftSqrTemple.style.scale = "0%";
+        leftSqrTemple.style.opacity = "0";
+        middleSqrTemple.style.scale = "0%";
+        middleSqrTemple.style.opacity = "0";
+        rightSqrTemple.style.scale = "0%";
+        rightSqrTemple.style.opacity = "0";
+    });
+
     buttonTemple.addEventListener("click",function(){
         templeOverlay.style.display ="none";
         shelf.style.display="block";
@@ -168,6 +204,15 @@
         rightSqrMercado.style.scale="100%";
         rightSqrMercado.style.opacity="1";
     });
+    heartMercado.addEventListener("mouseleave", function() {
+        leftSqrMercado.style.scale = "0%";
+        leftSqrMercado.style.opacity = "0";
+        middleSqrMercado.style.scale = "0%";
+        middleSqrMercado.style.opacity = "0";
+        rightSqrMercado.style.scale = "0%";
+        rightSqrMercado.style.opacity = "0";
+    });
+
     buttonMercado.addEventListener("click",function(){
         mercadoOverlay.style.display ="none";
         shelf.style.display="block";
@@ -182,6 +227,16 @@
         rightSqrArb.style.scale="100%";
         rightSqrArb.style.opacity="1";
     });
+
+    heartArb.addEventListener("mouseleave", function() {
+        leftSqrArb.style.scale = "0%";
+        leftSqrArb.style.opacity = "0";
+        middleSqrArb.style.scale = "0%";
+        middleSqrArb.style.opacity = "0";
+        rightSqrArb.style.scale = "0%";
+        rightSqrArb.style.opacity = "0";
+    });
+
     buttonArb.addEventListener("click",function(){
         arbOverlay.style.display ="none";
         shelf.style.display="block";
